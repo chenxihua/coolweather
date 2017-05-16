1eb7985a8e1ecc98ac59ae915e40aaae87dae1e5
chen <2642446787@qq.com>
2017-05-16 23:34:15 +0800

    今晚到这里正吧！累了。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+
+}
